6e48fb3a0e07242322c0f5c203ae9fba37272333
ryan clayson <ryanclayson@hotmail.com>
2020-11-11 10:40:20 -0500

    more changes

--- src/ca/durhamcollege/Main.java
+++ src/ca/durhamcollege/Main.java
@@ -1,8 +1,33 @@
+/*
+ * Author
+ * Date
+
+ */
+
 package ca.durhamcollege;
+import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args)
+    {
+        // define the scanner
+        Scanner keyboard = new Scanner(System.in);
+
+        //Input for name
+        String name = null;
+        System.out.print("Please enter your name: ");
+        name = keyboard.nextLine();
+
+        //Input for age
+        float age = 0.0f;
+        System.out.print("Please enter your age: ");
+        age = keyboard.nextFloat();
+        System.out.println();
+
+        //Output
+        System.out.printf("You Entered: %s \n", name);
+        System.out.printf("You Entered: %.2f \n", age);
+
     }
 }
